bf45d4a74edc701d9bb56d6320cfcbb1f21096b6
Zac <zmiranda@uchicago.edu>
2025-12-01 06:18:31 -0600

    adjusting from local pathing

--- dna_plays_pokemon.py
+++ dna_plays_pokemon.py
@@ -12,14 +12,14 @@ def iter_fasta(path:str):
             
 
 # loading best cfg
-with open('/home/zac/Desktop/uchicago/science_computing/final_project/best_cfg.pkl', 'rb') as f_in:
+with open('./best_cfg.pkl', 'rb') as f_in:
     best_cfg = pickle.load(f_in)
     
 # loading data
-dna_gen = iter_fasta('/home/zac/Desktop/uchicago/science_computing/final_project/lung_fish_data/GCA_040581445.1_ASM4058144v1_genomic.fna')
+dna_gen = iter_fasta('./lung_fish_data/GCA_040581445.1_ASM4058144v1_genomic.fna')
     
 # starting DNABoy
-boy = DNABoy('/home/zac/Desktop/uchicago/science_computing/final_project/pokemon_red.gb', '/home/zac/Desktop/uchicago/science_computing/final_project/game_start.state')
+boy = DNABoy('./pokemon_red.gb', './game_start.state')
 
 # execute code
 for line in dna_gen:
